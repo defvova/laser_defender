3fbb71c5315e02976eeb01b9d75a52827409cbb2
Vova <volodimir.partytskyi@gmail.com>
2020-06-30 18:02:46 +0300

    Add ViewportToWorldPoint to setup Boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,21 +6,57 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float acceleration = 10f;
+    [SerializeField] float padding = 0.5f;
 
     private Vector2 motion = new Vector2(0, 0);
+
+    private Camera mainCamera;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
     private readonly string axisHorizontalName = "Horizontal";
     private readonly string axisVerticalName = "Vertical";
 
-    void Update()
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        SetUpBoundaries();
+    }
+
+    private void SetUpBoundaries()
+    {
+        minX = GetBoundaries().x + padding;
+        maxX = GetBoundaries(1).x - padding;
+
+        minY = GetBoundaries().y + padding;
+        maxY = GetBoundaries(0, 1).y - padding;
+    }
+
+    private Vector3 GetBoundaries(int x = 0, int y = 0, int z = 0)
+    {
+        return mainCamera.ViewportToWorldPoint(new Vector3(x, y, z));
+    }
+
+    private void Update()
     {
         Move();
     }
 
     private void Move()
     {
-        motion.x = GetVelocity(axisHorizontalName) + transform.position.x;
-        motion.y = GetVelocity(axisVerticalName) + transform.position.y;
+        float motionX = GetMotion(axisHorizontalName, 0);
+        float motionY = GetMotion(axisVerticalName, 1);
+        motion.x = Mathf.Clamp(motionX, minX, maxX);
+        motion.y = Mathf.Clamp(motionY, minY, maxY);
         transform.position = motion;
+    }
+
+    private float GetMotion(string axisName, int vectorIndex)
+    {
+        return GetVelocity(axisName) + transform.position[vectorIndex];
     }
 
     private float GetVelocity(string axisName)
