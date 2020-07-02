234857528f5d2faa8ab5c6622b1d18154af89c61
Vova <volodimir.partytskyi@gmail.com>
2020-07-02 15:26:56 +0300

    Add enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<Waveconfig> waveConfigs;
+    int startingWave = 0;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnAllEnemiesInWave(CurrentWave()));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(Waveconfig waveConfig)
+    {
+        for(int index = 0; index < waveConfig.GetNumberOfEnemies(); index++)
+        {
+            Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+    private Waveconfig CurrentWave()
+    {
+        return waveConfigs[startingWave];
+    }
+}
